2850c97e82e8378e09a6fb0832236ab71686081b
abdulahad08 <abdulahadjut125@gmail.com>
2023-09-01 06:35:02 -0700

    Js Assignemnt

---
+++ app.js
@@ -0,0 +1,62 @@
+                                             //------------Chapter no 1-----------//
+
+// Q1 Answer//
+alert ("Hello Welcome");
+
+// Q2 Answer //
+alert("Error! Please enter a valid password.!");
+
+// Q3 Answer //
+alert("Welcome to JS land..! \nHappy coding!");
+
+// Q4 Answer //
+alert ("Welcome to JS land....");
+alert ("Happy coding.!\n ▢ prevent this page from creating additional dialog");
+
+// Q5 Answer //
+let text1;
+let text2= prompt("Please enter your Message :");
+alert (text2);
+
+
+              //------------Chapter no 2-----------//
+
+// Q 1 Answer //
+var username;
+
+// Q 2 Answer //
+var myName = "Abdul Ahad";
+console.log (myName);
+
+// Q 3 Answer //
+var message = "Hello World";
+alert (message);
+
+// Q 4 Answer //
+ var name="Jhone Doe";
+ var age= "I'm 19 Years old";
+ var aim= "I am certified Mobile App developer ";
+
+ alert (name);
+ alert (age);
+ alert (aim);
+
+ // Q 5 Answer //
+ var pizza="PIZZA\nPIZZ\nPIZ\nPI\nP";
+ alert(pizza);
+
+ // Q 6 Answer //
+ var email="abdulahadjutt11@gmail.com";
+ var text ="My Email adress is : ";
+ alert (text + email);
+
+// Q 7 Answer //
+var book ="I am trying to learn from the book A smarter way to learn JavaScript";
+alert (book);
+
+// Q 8 Answer //
+document.write("<h1>Yah! I Can write HTML content through JavaScript</h1>");
+
+// Q 9 Answer //
+var stylishLine="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(stylishLine);
